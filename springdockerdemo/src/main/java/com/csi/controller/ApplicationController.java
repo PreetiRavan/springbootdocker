7a7473806aa95a86e88b8d0608555f55fc320e96
unknown <ravanpreeti1998@gmail.com>
2021-12-29 22:47:24 +0530

    Initial Commit for spring boot

---
+++ springdockerdemo/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,23 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApplicationController {
+
+    @RequestMapping("/home")
+    public String sayHome()
+    {
+        return "welcome to csi";
+    }
+
+    @RequestMapping("/Address")
+    public String sayAddress()
+    {
+        return "Inspiria Mall | Pune | Nigdi";
+    }
+
+
+}
